5b686ad7120a7b93e0be22329e43fc03f1556dc2
Erik Nilsson <eeenilsson@users.noreply.github.com>
2023-10-21 23:05:24 +0200

    terminology

--- cherries_main.r
+++ cherries_main.r
@@ -3,10 +3,9 @@
 
 ## Quarto -----
 pacman::p_load(quarto)
-quarto_render('cherries_website/index.qmd')
-
-?quarto_render
-
+## quarto_render('cherries_website/index.qmd')
+## ?quarto_render
+getwd()
 quarto_render('cherries_website')
 
 ## quarto_render('test_presentation.qmd')
