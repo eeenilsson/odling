69acbcd1171e83dcc92c44f5d8afef880e0c2973
Erik Nilsson <eeenilsson@users.noreply.github.com>
2023-10-25 02:18:33 +0200

    redundant code.r

---
+++ redundant_code.r
@@ -0,0 +1,13 @@
+
+## from cherries_main
+
+## ## change to matching var
+## variety_genotype_group[, tempvar := gsub("guigne_dannonay", "annonay", tempvar)]
+## variety_genotype_group[, tempvar := gsub("allm_gulrod", "allman_gulrod", tempvar)]
+## variety_genotype_group[, tempvar := gsub("altenburger_melonen_kirsche", "buttners_rote", tempvar)]
+## variety_genotype_group[, tempvar := gsub("buttners_spate_rote_knorpelkirsche", "buttners_rote", tempvar)]
+## variety_genotype_group[, tempvar := gsub("fryksaas", "fryksas", tempvar)]
+## variety_genotype_group[, tempvar := gsub("gaardebo", "gardebo", tempvar)]
+## variety_genotype_group[, tempvar := gsub("grosse_schwarze_knorpel", "stor_svart", tempvar)]
+## variety_genotype_group[, tempvar := gsub("donissens_gelbe_knorpel", "donissen", tempvar)]
+## variety_genotype_group[, tempvar := gsub("frogmore_early", "frogmore", tempvar)]
