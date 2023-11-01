6da3e8e996ea58a05903dc977261ef85d33147fe
Erik Nilsson <eeenilsson@users.noreply.github.com>
2023-11-01 14:59:10 +0100

    compat tetraploid

--- cherry_compatibility.r
+++ cherry_compatibility.r
@@ -12,6 +12,7 @@ compat(genotype_a, genotype_b)
 compat("S4S8", "S1S3")
 genotype_a <- "S6S13ʹS26S36a" ## skuggmorell surkörsbär
 genotype_b <- "S1S6"
+compat(genotype_a, genotype_b)
 
 compat <- function(genotype_a, genotype_b){
     ## calculate relative compatibility
@@ -22,21 +23,40 @@ compat <- function(genotype_a, genotype_b){
     target <- strsplit(target, "S")[[1]][-1]
 
     ## check if terraploid (sour cherry)
-    tertaploid_pol <- ifelse(!grepl("/", genotype_a) & length(pollinator) == 4, TRUE, FALSE)
-    tertaploid_target <- ifelse(!grepl("/", genotype_b) & length(target) == 4, TRUE, FALSE)
+    tetraploid_pol <- ifelse(!grepl("/", genotype_a) & length(pollinator) == 4, TRUE, FALSE)
+    tetraploid_target <- ifelse(!grepl("/", genotype_b) & length(target) == 4, TRUE, FALSE)
     ## Note: Man har identifierat 12 fungerande S-haplotyper (S1, S4, S6, S9, S12, S13, S14, S16 ,S26, S33, S34, och S35) och nio icke-fungerande (S1ʹ, S6m, S6m2, S13ʹ, S13m, S36a , S36b, S36b2, och S36b3). Vissa finns även hos sörkörsbär (S1, S4, S6, S9, S12, S13, S 14, S16, och S34). The presence of two or more non-functional S-haplotypes within sour cherry 2x pollen renders that pollen SC.
-    
+    mutants <- c("ʹ$|'$|^6m$|^6m2$|^13m$|^36a$|^36b$|^36b2$|^36b3$")
+    target <- target[!grepl(mutants, target)] ## remove mutated S-alleles (does not confer GSI)
     check <- !grepl(paste0("^", paste0(target, collapse = "$|^"), "$"), pollinator)
-    sucess <- pmax(check, grepl("'|6m|6m2|13m|36a|36b|36b2|36b3", pollinator)) ## count as success if "'" or sour cherry mutants
+    sucess <- pmax(check, grepl("ʹ$|'$", pollinator)) ## count as success if "'"
     success_alt1 <- sucess[c(TRUE, TRUE, FALSE, FALSE)]
     success_alt2 <- sucess[!c(TRUE, TRUE, FALSE, FALSE)] ## NA if length 2
     comp <- sum(success_alt1, na.rm = TRUE)
     if(grepl("/", genotype_a)){
+        ## if uncertain genotype (/) use worst case
         comp <- min(sum(success_alt1, na.rm = TRUE), sum(success_alt2, na.rm = TRUE), na.rm = TRUE)
     }
-    if(tertaploid_pol){
-        comp <- sum(success_alt1, na.rm = TRUE) + sum(success_alt2, na.rm = TRUE),        
-        if(comp > 2){comp <- 2} ## count max 2 successes
+    if(tetraploid_pol){
+        ## mutants <- c("'", "6m", "6m2", "13m", "36a", "36b", "36b2", "36b3")
+        p_mut <- pollinator[grepl(mutants, pollinator)]
+        p_nonmut <- pollinator[!grepl(mutants, pollinator)]
+        p_nonmut
+        check_nonmut <- !grepl(paste0("^", paste0(target, collapse = "$|^"), "$"), p_nonmut)
+        p_nonmut_compat <- p_nonmut[check_nonmut]
+        p_compat <- c(p_mut, p_nonmut_compat) ## mut or absent in target
+        success_probability <- 0
+        if(length(p_compat) == 2){success_probability <- 2/8}
+        if(length(p_compat) == 3){success_probability <- 4/8}
+        if(length(p_compat) == 8){success_probability <- 8/8}
+        ## abcd => ab ac ad bc bd cd but this is not the case in meiosis, see youtube:
+        ## see https://www.youtube.com/watch?v=NttqS-N17FQ
+        ## possible number of gametes = 2^n        
+        ## where n is the haploid number (2n=4 => diploid nr=4, haplod nr=2)
+        ##  sour cherry is an allotetraploid (2n = 4x = 32)
+        ## if there is 1 mutant the probability of a sperm to contain 2 mutants is 0.
+        ## 2 mutants => probability of pollen to contain 2 mutants = 2/8, 3 m => 4/8, 4m => 8/8
+        comp <- ifelse(success_probability >0, ceiling(success_probability+0.1), 0)
     }
 
     return(comp)        
