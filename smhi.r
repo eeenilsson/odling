99f3236084fa16223be99ab2aa096d751cde0133
Erik Nilsson <eeenilsson@users.noreply.github.com>
2023-11-08 23:06:51 +0100

    smhi

--- smhi.r
+++ smhi.r
@@ -10,68 +10,76 @@ sites$name <- factor(sites$name)
 ## Data on airtemp measuring sites sweden
 length(sites$id) ## 920 sites
 
-chunks <- c(0, 100, 200, 300, 400, 500, 600, 700, 800, 900)
+## ## download temperature data in chunks (large data) ----------
+## chunks <- c(0, 100, 200, 300, 400, 500, 600, 700, 800, 900)
 
-## chunks <- c(0, 10, 20, 30)
-## n <- 1
-## n <- 3
-for(n in 1:length(chunks)){
-## loop over site id to get data
-    message(chunks[n])
-    smhi_airtemp <- data.table(date = NA, time = NA, temp = NA, quality = NA, id = NA)
+## ## chunks <- c(0, 10, 20, 30)
+## ## n <- 1
+## ## n <- 3
+## for(n in 1:length(chunks)){
+## ## loop over site id to get data
+##     message(chunks[n])
+##     smhi_airtemp <- data.table(date = NA, time = NA, temp = NA, quality = NA, id = NA)
 
-    from <- chunks[n]+1    
-    to <- ifelse(chunks[n] == max(chunks),
-                 ## 40,
-                 length(sites$id),
-                 chunks[n+1])
-for(i in sites$id[from:to]){
-    ## i <- sites$id[3]
-    ## i <- "98040"
-    ## i <- 66420
-    ## i <- 125490
-    ## Error in read.table("temp.txt", sep = ";") : no lines available in input
-    ## message(i)
-    ## min max (vanligen kl 06 och 18)
-    ## link <- paste0("https://opendata-download.smhi.se/stream?type=metobs&parameterIds=26,27&stationId=", i, "&period=corrected-archive") ## 12 h min max, note: check csv, not in order
+##     from <- chunks[n]+1    
+##     to <- ifelse(chunks[n] == max(chunks),
+##                  ## 40,
+##                  length(sites$id),
+##                  chunks[n+1])
+## for(i in sites$id[from:to]){
+##     ## Error in read.table("temp.txt", sep = ";") : no lines available in input
+##     ## min max (vanligen kl 06 och 18)
+##     ## link <- paste0("https://opendata-download.smhi.se/stream?type=metobs&parameterIds=26,27&stationId=", i, "&period=corrected-archive") ## 12 h min max, note: check csv, not in order
 
-    ## alla mätningar
-    link <- paste0("https://opendata-download.smhi.se/stream?type=metobs&parameterIds=1&stationId=", i, "&period=corrected-archive")
+##     ## alla mätningar
+##     link <- paste0("https://opendata-download.smhi.se/stream?type=metobs&parameterIds=1&stationId=", i, "&period=corrected-archive")
 
-    page_html <- rvest::read_html(link)
-    test <- page_html %>% rvest::html_nodes("body")
-    text <- html_text(test)
-    text <- gsub("^.* Dessa levereras ej..", "", text) ## remove intro
-    ## example <- "1858-12-05;13:00:00;1.4;G\n1858-12-05;20:00:00;0.6;G\n1858-12-06;07:00:00;0.8;G\n"
-    ## writeLines(example, "example.txt")
-    if(text != ""){ ## test if empty source
-        writeLines(text, "temp.txt")
+##     page_html <- rvest::read_html(link)
+##     test <- page_html %>% rvest::html_nodes("body")
+##     text <- html_text(test)
+##     text <- gsub("^.* Dessa levereras ej..", "", text) ## remove intro
+##     ## example <- "1858-12-05;13:00:00;1.4;G\n1858-12-05;20:00:00;0.6;G\n1858-12-06;07:00:00;0.8;G\n"
+##     ## writeLines(example, "example.txt")
+##     if(text != ""){ ## test if empty source
+##         writeLines(text, "temp.txt")
 
-        ## if(i == 98040){
-        ##     incomplete <- readLines("temp.txt", n = 376019) ## line 376020 incomplete
-        ##     writeLines(incomplete, "temp.txt")
-        ##     message(paste0(i, " has an incomplete line"))
-        ## }
+##         ## if(i == 98040){
+##         ##     incomplete <- readLines("temp.txt", n = 376019) ## line 376020 incomplete
+##         ##     writeLines(incomplete, "temp.txt")
+##         ##     message(paste0(i, " has an incomplete line"))
+##         ## }
         
-        ##     if(i == 74180){
-        ##             incomplete <- readLines("temp.txt", n = 375805) ## line 375806 did not have 4 elements
-        ##     writeLines(incomplete, "temp.txt")
-        ##     message(paste0(i, " has an incomplete line"))
+##         ##     if(i == 74180){
+##         ##             incomplete <- readLines("temp.txt", n = 375805) ## line 375806 did not have 4 elements
+##         ##     writeLines(incomplete, "temp.txt")
+##         ##     message(paste0(i, " has an incomplete line"))
 
-        ## } ## Note: using fill = TRUE instead. Seems to be last line
-        result <- read.table("temp.txt", sep = ";", fill = TRUE)
-        names(result) <- c("date", "time", "temp", "quality")
-        ## result[376090:376094, ]
-        ## nrow(result)
-        result$id <- i
-        smhi_airtemp <- rbind(smhi_airtemp, result)
-    }
-}
-    smhi_airtemp <- smhi_airtemp[!is.na(date), ]
-    saveRDS(smhi_airtemp, paste0("smhi_airtemp_", to, ".rds"))
-    }
-
+##         ## } ## Note: using fill = TRUE instead. Seems to be last line
+##         result <- read.table("temp.txt", sep = ";", fill = TRUE)
+##         names(result) <- c("date", "time", "temp", "quality")
+##         ## result[376090:376094, ]
+##         ## nrow(result)
+##         result$id <- i
+##         smhi_airtemp <- rbind(smhi_airtemp, result)
+##     }
+## }
+##     smhi_airtemp <- smhi_airtemp[!is.na(date), ]
+##     saveRDS(smhi_airtemp, paste0("smhi_airtemp_", to, ".rds"))
+##     }
 ## test <- readRDS("smhi_airtemp_20.rds")
+
+## read data stored in .rds
+
+airtemp <- readRDS("smhi_airtemp_100.rds")
+airtemp <- as.data.table(airtemp)
+tmp <- c(200, 300, 400, 500, 600, 700, 800, 900)
+for(i in tmp){
+dt_tmp <- readRDS(paste0("smhi_airtemp_", i, ".rds"))
+dt_tmp <- as.data.table(dt_tmp)
+airtemp <- rbind(airtemp, dt_tmp)
+}
+
+str(airtemp)
 
 ## join with sites info
 ## Note: only add label to reduce size of data?
